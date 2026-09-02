f8eb64d066220d15ce4b4c208ca36b77de12d585
benzyplug <codeelite24@gmail.com>
2026-09-02 17:16:59 +0100

    fix: simplify Discord channel type augmentation

--- src/types/discordjs.d.ts
+++ src/types/discordjs.d.ts
@@ -1,7 +1,5 @@
-import type { MessageCreateOptions, MessagePayload } from 'discord.js';
-
 declare module 'discord.js' {
   interface PartialGroupDMChannel {
-    send(options: string | MessagePayload | MessageCreateOptions): Promise<import('discord.js').Message>;
+    send(...args: any[]): Promise<any>;
   }
 }
